79d338994ea66b950375e2dcc1b436b3d3f788d1
adnan-abdullah <adnan.abdullah@lelafe.com>
2025-04-06 14:59:30 +0530

    added package db

--- backend/src/firebase/firebase/firebase.service.ts
+++ backend/src/firebase/firebase/firebase.service.ts
@@ -5,29 +5,8 @@ import { app } from 'firebase-admin';
 export class FirebaseService {
   constructor(@Inject('FIREBASE_APP') private firebaseApp: app.App) {}
 
-  async uploadImage(
-    filePath: string,
-    destination: string = 'gs://dazzling-seat-362406.appspot.com/tour images',
-  ): Promise<string> {
-    const bucket = this.firebaseApp
-      .storage()
-      .bucket('dazzling-seat-362406.appspot.com');
-
-    try {
-      // Upload the file to Firebase Storage
-      await bucket.upload(filePath, {
-        destination,
-        public: true, // Make the file publicly accessible,
-        metadata: {
-          contentType: 'images/png',
-        },
-      });
-
-      // Generate the public URL
-      const publicUrl = `https://storage.googleapis.com/${bucket.name}/${destination}`;
-      return publicUrl;
-    } catch (error) {
-      throw new Error(`Failed to upload image: ${error.message}`);
-    }
+  async uploadImage(filePath: string, destination: string): Promise<string> {
+    const bucket = this.firebaseApp.storage().bucket();
+    return Promise.resolve() as unknown as string;
   }
 }
